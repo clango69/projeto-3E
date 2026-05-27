9294bc13ebfcd097cd7e44280929166c25e12638
Nicolas <nicolaspereiralopes114@gmail.com>
2026-05-27 16:21:54 -0300

    Update alunos.php

--- alunos.php
+++ alunos.php
@@ -1,4 +1,9 @@
 <?php
+// ATIVA EXIBIÇÃO DE ERROS PARA DETECTAR O BUG NO RAILWAY
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 include 'conexao.php';
 $msg = ''; $tipo_msg = '';
 
